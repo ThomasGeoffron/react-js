96de0d2351da22a3ce8d05bfb2abb59745c0e091
Thomas GEOFFRON <tgeoffron@myges.fr>
2022-02-08 12:03:48 +0100

    Feat: First draft of createElement function

--- www/app/MiniReact.js
+++ www/app/MiniReact.js
@@ -1,7 +1,13 @@
+const events = ["click", "hover", "focus", "change", "keyup"];
+
 const MiniReact = {
     Component: class Component {
         oldProps = null;
         state = {};
+
+        constructor(initProps) {
+            this.oldProps = initProps;
+        }
 
         shouldUpdate(newProps) {
             return JSON.stringify(this.oldProps) !== JSON.stringify(newProps);
@@ -15,7 +21,37 @@ const MiniReact = {
         }
 
     },
-    createElement: function () {
+    createElement: function (element) {
+        if (typeof element.type === "string") {
+            let node = document.createElement(element.type);
+            let props = element.props;
+            let children = element.children;
 
+            if (props) {
+                for (let prop in props) {
+                    if (events.includes(prop)) {
+                        node.addEventListener(prop, props[prop]);
+                    } else {
+                        node.setAttribute(prop, props[prop]);
+                    }
+                }
+            }
+            if (children) {
+                for (let child of children) {
+                    if (child === undefined) continue;
+                    if (typeof child === "string") {
+                        node.appendChild(
+                            document.createTextNode(child)
+                        );
+                    } else {
+                        node.appendChild(this.createElement(child))
+                    }
+                }
+            }
+            return node;
+        }
+        else {
+            //TODO: Management of component creation
+        }
     }
 };
