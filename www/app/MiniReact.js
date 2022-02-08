45e29220eb16155e81a3c4e99d7431ae54f684ed
Thomas GEOFFRON <tgeoffron@myges.fr>
2022-02-08 13:11:21 +0100

    Fix: change createElement parameters

--- www/app/MiniReact.js
+++ www/app/MiniReact.js
@@ -1,6 +1,6 @@
 const events = ["click", "hover", "focus", "change", "keyup"];
 
-const MiniReact = {
+export const MiniReact = {
     Component: class Component {
         oldProps = null;
         state = {};
@@ -16,20 +16,20 @@ const MiniReact = {
         display(newProps = null) {
             if (this.shouldUpdate(newProps)) {
                 this.oldProps = newProps;
-                this.render();
             }
+            return this.render();
         }
 
     },
-    createElement: function (element) {
-        if (typeof element.type === "string") {
-            let node = document.createElement(element.type);
-            let props = element.props;
-            let children = element.children;
+    createElement: function (element, props, children) {
+
+        if (typeof element === "string") {
+            let node = document.createElement(element);
 
             if (props) {
                 for (let prop in props) {
                     if (events.includes(prop)) {
+                        console.log(prop);
                         node.addEventListener(prop, props[prop]);
                     } else {
                         node.setAttribute(prop, props[prop]);
@@ -38,13 +38,14 @@ const MiniReact = {
             }
             if (children) {
                 for (let child of children) {
+                    console.log(typeof child)
                     if (child === undefined) continue;
                     if (typeof child === "string") {
                         node.appendChild(
                             document.createTextNode(child)
                         );
                     } else {
-                        node.appendChild(this.createElement(child))
+                        node.appendChild(child);
                     }
                 }
             }
@@ -52,6 +53,9 @@ const MiniReact = {
         }
         else {
             //TODO: Management of component creation
+            let component = new element(props);
+
+            return component.display(props);
         }
     }
 };
