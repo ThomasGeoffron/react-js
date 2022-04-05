22392114a58e66089cc99c420a27d502542fbb28
Thomas GEOFFRON <tgeoffron@myges.fr>
2022-04-05 21:38:48 +0200

    Importing libs

--- www/main.js
+++ www/main.js
@@ -1,10 +1,11 @@
 const root = document.getElementById("root");
 import {MiniReact} from "./app/MiniReact.js";
 import {Home} from "./app/components/Home.js";
+import {interpolate} from "./utils/Interpolate";
+import {prop_access} from "./utils/PropAccess";
 
-String.prototype.interpolate = function () {
-  return this;
-};
+String.prototype.interpolate = interpolate;
+Object.prototype.prop_access = prop_access;
 
 root.appendChild(
     MiniReact.createElement(Home)
